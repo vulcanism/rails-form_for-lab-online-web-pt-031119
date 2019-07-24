8abd20145294efb65cb2dac2755a3b7379dcd2a9
Savannah C <sgcorrell@gmail.com>
2019-07-24 19:24:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,6 +1,6 @@
-class SchoolClasses < ActiveRecord::Migration[5.0]
+class CreateSchoolClasses < ActiveRecord::Migration[5.0]
   def change
-    create_table :school_class do |t|
+    create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
     end
